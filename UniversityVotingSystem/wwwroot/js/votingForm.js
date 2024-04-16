462a3df5267d12d098757b67df1b5a0a13407306
Kirikosha <kirikoshamain@gmail.com>
2024-04-16 20:57:14 +0200

    Added validation to the voting creation

--- UniversityVotingSystem/wwwroot/js/votingForm.js
+++ UniversityVotingSystem/wwwroot/js/votingForm.js
@@ -1,21 +1,32 @@
-function createProposition(){
+const SIZE = 3;
+
+function createProposition()
+{
     var propositionContainer = document.getElementById("create-voting");
     var propositionHTMLCollection = document.getElementsByClassName("voting-propositions");
-    var lastIndexVar = "proposition-" + lastIndex(propositionHTMLCollection);
+    if (lastIndex(propositionHTMLCollection) > 10)
+    {
+        alert("There can't be > than 10 propositions!!!")
+    }
+    else
+    {
+        var lastIndexVar = "proposition-" + lastIndex(propositionHTMLCollection);
 
-    //Div and input initialization
-    var innerDiv = document.createElement("div");
-    innerDiv.className = "voting-propositions";
-    innerDiv.id = lastIndexVar;
-    var innerInput = document.createElement("input");
-    innerInput.type = "text";
-    innerInput.placeholder = "Proposition name";
-
-    innerDiv.appendChild(innerInput);
-    propositionContainer.appendChild(innerDiv);
+        //Div and input initialization
+        var innerDiv = document.createElement("div");
+        innerDiv.className = "voting-propositions";
+        innerDiv.id = lastIndexVar;
+        var innerInput = document.createElement("input");
+        innerInput.type = "text";
+        innerInput.placeholder = "Proposition name";
+    
+        innerDiv.appendChild(innerInput);
+        propositionContainer.appendChild(innerDiv);
+    }
 }
 
-function lastIndex(collection){
+function lastIndex(collection)
+{
     var lastElement = collection[collection.length - 1];
     var idString = lastElement.getAttribute("id");
     var splittedId = idString.split('-');
@@ -26,7 +37,8 @@ function lastIndex(collection){
     return 2;
 }
 
-function deleteLastProposition(){
+function deleteLastProposition()
+{
     var propositionHTMLCollection = document.getElementsByClassName("voting-propositions");
     var lastIndexVar = lastIndex(propositionHTMLCollection) - 1;
     if(lastIndexVar > 1){
@@ -38,18 +50,24 @@ function deleteLastProposition(){
     }
 }
 
-function submitVoting(){
+function submitVoting()
+{
     const propositionHTMLCollection = document.getElementsByClassName("voting-propositions");
     const propositionArray = Array.from(propositionHTMLCollection);
     var propositionValues = filterPropositions(propositionArray);
     var votingName = document.getElementById("votingName").value;
     console.log(votingName);
-    var jsonString = getJson(votingName, propositionValues);
-    console.log(jsonString);
-    makePostRequest(jsonString);
+    var isValid = validationStarter(votingName, propositionValues);
+    if (isValid)
+    {
+        var jsonString = getJson(votingName, propositionValues);
+        console.log(jsonString);
+        makePostRequest(jsonString);
+    }
 }
 
-function filterPropositions(propositionArray){
+function filterPropositions(propositionArray)
+{
     var values = new Array();
     for(i = 0; i < propositionArray.length; i++){
         var inputValue = propositionArray[i].children[0].value;
@@ -60,7 +78,8 @@ function filterPropositions(propositionArray){
     return values;
 }
 
-function getJson(votingName, propositionValues){
+function getJson(votingName, propositionValues)
+{
     
     var jsonObj = {
         name : votingName,
@@ -70,7 +89,8 @@ function getJson(votingName, propositionValues){
     return jsonString;
 }
 
-function makePostRequest(jsonString){
+function makePostRequest(jsonString)
+{
     var requestOptions = {
         method: 'POST',
         headers: {
@@ -85,8 +105,93 @@ function makePostRequest(jsonString){
     });
 }
 
-function getToken(){
-    var token = '@Html.AntiForgeryToken()';
-    token = $(token).val();
-    return token;
+function validationStarter(votingName, propositionArray)
+{
+    var validateVotingNameErrorMessage = validateVotingName(votingName);
+    var validatePropositionErrorMessage = validateProposition(propositionArray);
+
+    var errorMessage = validateVotingNameErrorMessage + validatePropositionErrorMessage;
+    console.log(errorMessage);
+    if(errorMessage != "")
+    {
+        alert(errorMessage);
+        return false; //operation did not pass due to errors
+    }
+    return true; // operation passed
+}
+
+function validateVotingName(votingName)
+{
+    var errorMessage = "";
+    let sizeError = false;
+    let capitalLetterError = false;
+
+    if (votingName.length < SIZE)
+    {
+        sizeError = true;
+    }
+    
+    let firstLetter = votingName.charAt(0);
+    if (firstLetter != firstLetter.toUpperCase())
+    {
+        capitalLetterError = true;
+    }
+
+    var errorMessage = createVotingNameErrorMessage(sizeError, capitalLetterError);
+    return errorMessage;
+}
+
+function validateProposition(propositionArray)
+{
+    if (propositionArray.length == 0)
+    {
+        var errorMessage = "\nThere should be at least 1 proposition";
+        return errorMessage;
+    }
+    var propositionsWithErrors = new Array();
+    for (var i = 0; i < propositionArray.length; i++)
+    {
+        if (propositionArray[i].length < SIZE)
+        {
+            propositionsWithErrors.push(propositionArray[i]);
+        }
+    }
+
+    var errorMessage = ""; 
+    if (propositionsWithErrors.length > 0)
+    {
+        errorMessage = createPropositionsErrorMessage(propositionsWithErrors);
+    }
+
+    return errorMessage;
+}
+
+function createPropositionsErrorMessage(propositionsWithErrors)
+{
+    var errorMessage = "\nYou have written wrong propositions. Their length is smaller than 3. They are:";
+    for (var i = 0; i  < propositionsWithErrors.length; i++)
+    {
+        errorMessage+= "\n" + propositionsWithErrors[i];
+    }
+    return errorMessage;
+}
+
+function createVotingNameErrorMessage(sizeError, capitalLetterError)
+{
+    if (!sizeError && !capitalLetterError)
+    {
+        return "";
+    }
+    var errorMessage = "You have made several mistakes in voting name. They are:";
+
+    if (sizeError)
+    {
+        errorMessage += "\nSize of voting name should be > 3";
+    }
+
+    if (capitalLetterError)
+    {
+        errorMessage += "\nFirst letter of the voting name should be uppercase";
+    }
+    return errorMessage;
 }
